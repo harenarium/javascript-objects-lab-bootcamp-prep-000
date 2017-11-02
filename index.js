b39f242669b6bceb2f9c1fd8227cf28daf4fe52a
harenarium <carolinel2610@gmail.com>
2017-11-02 23:14:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object,{[key]:value})
 }
+
+function deleteFromObjectByKey(object, key){
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+}
