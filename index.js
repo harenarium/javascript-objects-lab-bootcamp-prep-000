c7fdac64635b17c22ff7234fd6261e25344420e2
harenarium <carolinel2610@gmail.com>
2017-11-02 23:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var recipes ={}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObject = Object.assign({},object,{[key]:value})
+  return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object,{[key]:value})
+}
